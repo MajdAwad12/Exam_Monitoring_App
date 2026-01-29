9b00de51ee977e3423aba7df5300d8419335ae46
MajdAwad12 <majdawad86@gmail.com>
2026-01-30 01:32:18 +0200

    o

--- Exam_monitoring_App_HW3/client/src/components/auth/AuthFooter.jsx
+++ Exam_monitoring_App_HW3/client/src/components/auth/AuthFooter.jsx
@@ -1,7 +1,7 @@
 // client/src/components/auth/AuthFooter.jsx
 export default function AuthFooter() {
   return (
-    <footer className="text-center text-xs text-white-500">
+    <footer className="text-center text-xs text-white-1000">
       Braude College • Web Course • Exam Monitoring Project
     </footer>
   );
