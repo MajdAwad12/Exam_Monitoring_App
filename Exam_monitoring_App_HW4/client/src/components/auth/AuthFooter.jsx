e544caf7063a3b4269e4821a8932646b447ed79b
MajdAwad12 <majdawad86@gmail.com>
2026-02-09 09:25:45 +0200

    del hw4

--- Exam_monitoring_App_HW4/client/src/components/auth/AuthFooter.jsx
+++
@@ -1,8 +0,0 @@
-// client/src/components/auth/AuthFooter.jsx
-export default function AuthFooter() {
-  return (
-    <footer className="text-center text-xs text-white font-semibold">
-      Braude College  • Exam Monitoring App
-    </footer>
-  );
-}
